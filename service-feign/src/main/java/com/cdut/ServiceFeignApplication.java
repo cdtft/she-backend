5de97fca8a8c994eba4aba9764023500e467ab8e
wangcheng <453451180@qq.com>
2017-09-16 21:57:37 +0800

    添加微服务

--- service-feign/src/main/java/com/cdut/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/cdut/ServiceFeignApplication.java
@@ -2,11 +2,21 @@ package com.cdut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+@EnableDiscoveryClient
+@EnableHystrixDashboard
 public class ServiceFeignApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceFeignApplication.class, args);
 	}
 }
