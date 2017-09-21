a5e1b30a6be260065cfcf133c7916702758b440c
wangcheng <453451180@qq.com>
2017-09-21 18:11:47 +0800

    delete product service and impl

--- dao/src/main/java/com/cdut/dao/mysql/po/admin/Role.java
+++ dao/src/main/java/com/cdut/dao/mysql/po/admin/Role.java
@@ -32,5 +32,29 @@ public class Role extends BaseEntity{
      */
     private String description;
 
+    @Override
+    public Long getId() {
+        return id;
+    }
 
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- manager/src/main/java/com/cdut/controller/product/ProductController.java
+++ manager/src/main/java/com/cdut/controller/product/ProductController.java
@@ -1,12 +1,8 @@
 package com.cdut.controller.product;
 
-import com.cdut.dao.mysql.po.product.ProductPo;
-import com.cdut.service.product.ProductService;
 import com.cdut.soa.client.product.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,15 +15,6 @@ public class ProductController {
     @Autowired
     private ProductFeignClient productFeignClient;
 
-    @Autowired
-    private ProductService productService;
-
-    @RequestMapping("/findByName/{name}")
-    @ResponseBody
-    public ProductPo findByName(@PathVariable("name") String name) {
-
-        return productService.findByName(name);
-    }
 
     @RequestMapping("test")
     public String test() {
